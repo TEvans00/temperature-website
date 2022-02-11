1ea263ffd3739e1a13d10f8590fc95d1b71ead65
Tyla Merrill <tylamerrill@gmail.com>
2022-02-11 15:32:29 -0700

    Update URL to use remote hosting

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  EVENT_URL: 'http://localhost:3000/sky/event/ckzg7m6jv00057swe9v81drfy/none',
-  QUERY_URL: 'http://localhost:3000/sky/cloud/ckzg7m6jv00057swe9v81drfy'
+  EVENT_URL: 'http://18.117.110.129:3000/sky/event/ckziy4nxj001pu7qna6ui98ww/none',
+  QUERY_URL: 'http://18.117.110.129:3000/sky/cloud/ckziy4nxj001pu7qna6ui98ww'
 };
 
 export default config;
